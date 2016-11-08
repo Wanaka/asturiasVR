70e2c8382ef581e1a4c809161b6a7236eb87a059
wanaka <jonas.haag@hotmail.com>
2016-11-08 18:46:40 +0100

    working with fog

---
+++ Asturias VR/Assets/Scripts/cloud_movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cloud_movement : MonoBehaviour {
+
+
+ public static float movespeed = Random.Range(-0.2f, 0.2f);
+
+ public Vector3 userDirection = Vector3.left;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+transform.Translate(Vector3.forward * movespeed * Time.deltaTime);
+	}
+}
+
+
+    
