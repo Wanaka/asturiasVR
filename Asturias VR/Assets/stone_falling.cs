693eea5c35b150b9f9b483b8f18b9029e600c4f3
wanaka <jonas.haag@hotmail.com>
2016-11-14 20:25:40 +0100

    added walking sound and falling rocks

---
+++ Asturias VR/Assets/stone_falling.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stone_falling : MonoBehaviour {
+
+private AudioSource audio;
+
+    void Start(){
+            audio = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+    	audio.Play();  
+    }
+}
