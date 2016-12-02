dd7e4888d2d464e155c021238343a31c4bd6be63
wanaka <jonas.haag@hotmail.com>
2016-12-02 18:05:33 +0100

    Added character and mouse look script that works! also removed old stuff

---
+++ Asturias VR/Assets/character_controller_script.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class character_controller_script : MonoBehaviour {
+
+public float speed = 1.0F;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float translation = Input.GetAxis("Vertical") * speed;
+		float straffe = Input.GetAxis("Horizontal") * speed;
+		translation*= Time.deltaTime;
+		straffe *= Time.deltaTime;
+
+		transform.Translate(straffe,0,translation);
+		
+
+	}
+}
---
+++ Asturias VR/Assets/mouse_look_script.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouse_look_script : MonoBehaviour {
+
+	public float lookSens = 5f;
+	public float xRot;
+	public float yRot;
+
+	GameObject character;
+
+	// Use this for initialization
+	void Start () {
+		character = this.transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		xRot -= Input.GetAxis("Mouse Y") *lookSens;
+		yRot += Input.GetAxis("Mouse X") *lookSens;
+
+		//makes the mouse stop on y axis when it goes to far up and down
+		xRot = Mathf.Clamp(xRot, -90, 90);
+		character.transform.rotation = Quaternion.Euler(xRot, yRot, 0);
+
+	}
+
+}
+
+
+/*
+using UnityEngine;
+using System.Collections;
+
+public class mouse_look_script : MonoBehaviour {
+
+	Vector2 mouseLook;
+	Vector2 smoothV;
+	public float sensetivity = 5.0f;
+	public float smoothing =2.0f;
+
+	 GameObject character;
+	// Use this for initialization
+	void Start () {
+		character = this.transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+		md = Vector2.Scale(md, new Vector2(sensetivity * smoothing, sensetivity * smoothing));
+		smoothV.x = Mathf.Lerp(smoothV.x, md.x, 0.5f / smoothing);
+		smoothV.y = Mathf.Lerp(smoothV.y, md.y, 0.5f / smoothing);
+		mouseLook+= smoothV;
+
+
+		transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+		character.transform.localRotation = Quaternion.Euler(mouseLook.x, character.transform.up);
+	}
+}
+
+*/
