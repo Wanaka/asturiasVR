a520521f17e27826b243be167fe5e44397f042e5
wanaka <jonas.haag@hotmail.com>
2016-12-08 14:26:35 +0100

    rocks falling down the fallside!

---
+++ Asturias VR/Assets/drop_rocks.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drop_rocks : MonoBehaviour
+{
+    private int numSelectors = 20;
+    public GameObject brick; //selected in the editor
+    
+    void Start(){ 
+        
+    }
+
+
+    public void Inst(){
+        for (int i = 0; i < numSelectors; i++)
+        {
+                Instantiate(brick, new Vector3(-8.552f+(i*0.001f), 2.43f+(i*0.001f), 1f+(i*0.1f)), Quaternion.identity);            
+        }
+        
+    }
+}
--- Asturias VR/Assets/stone_falling.cs
+++ Asturias VR/Assets/stone_falling.cs
@@ -3,18 +3,30 @@ using System.Collections;
 
 public class stone_falling : MonoBehaviour {
 
+private drop_rocks droprocks;
 private AudioSource audio;
+//private bool hasEntered = false;
+
+void Awake ()
+    {
+    	droprocks = GameObject.FindObjectOfType<drop_rocks>();
+    }
 
     void Start(){
             audio = GetComponent<AudioSource>();
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
 	void OnTriggerEnter(Collider other) {
+
+		
     	audio.Play();  
+    	//Instantiate the rocks from the other script
+    	droprocks.Inst();
+    	
     }
 }
