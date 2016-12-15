fe6489a190df2f52a1ffc9337dca5ad4e17bc1af
wanaka <jonas.haag@hotmail.com>
2016-12-15 20:01:18 +0100

    Added scene, added anti-fall blocks, UI

---
+++ Asturias VR/Assets/Scripts/startScene.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class startScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnStartGame(){
+
+		//load asturias scene
+		SceneManager.LoadScene("asturias_scene");
+	}
+}
+
+
+
+
+  
