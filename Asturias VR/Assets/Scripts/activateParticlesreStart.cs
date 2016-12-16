92482b842fda3ea623bf7afb7e56dce9024dcdf5
wanaka <jonas.haag@hotmail.com>
2016-12-16 15:39:12 +0100

    Added end-site,  improved font and UI, better fog

---
+++ Asturias VR/Assets/Scripts/activateParticlesreStart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activateParticlesreStart : MonoBehaviour {
+
+public Transform particles;
+	// Use this for initialization
+	void Start () {
+		particles.GetComponent<ParticleSystem> ().enableEmission = false;
+	}
+	
+
+	void OnTriggerEnter(Collider other) {
+		particles.GetComponent<ParticleSystem> ().enableEmission = true;
+    	
+    }			
+}
